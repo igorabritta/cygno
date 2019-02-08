0ce97ea065f20e2092803bb8e8c073277dfe907e
Igor Abritta <igorabritta@gmail.com>
2019-02-08 15:18:54 +0000

    my first commit

---
+++ iDBSCAN.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+iDBSCAN: Iterative Density-Based Spatial Clustering of Applications with Noise
+"""
+
+import numpy as np
+from sklearn.cluster import DBSCAN
+
+def idbscan(X, iterative = 3):
+    """
+    Returns
+    -------
+    core_samples : array [n_core_samples]
+        Indices of core samples.
+    labels : array [n_samples]
+        Cluster labels for each point.  Noisy samples are given the label -1.
+    tag : array [n_samples]
+        tag for each point.
+            - Noisy samples are given the label 'n'
+            - Long tracks samples are given the label 'l'
+            - Medium tracks are given the label 'm'
+            - Small tracks are given the label 's'
+
+    """
+
+    ## - - - - -
+    Index              = np.arange(0,np.shape(X)[0],dtype=int)
+    Fcluster           = (-1)+np.zeros(np.shape(X)[0],dtype=int)
+    Flabel             = np.empty(np.shape(X)[0],dtype=str)
+    Flabel[:]          = 'n'
+    auxClu             = -1
+    # - - - - - -
+    vector_eps         = [2.26, 3.5, 2.8, 6]
+    vector_min_samples = [2, 30, 6, 2]
+    auxIti             = - 1
+    ## - - - - -
+
+    if iterative >= 0:
+
+        auxIti += 1
+        db      = DBSCAN(eps=vector_eps[auxIti], min_samples=vector_min_samples[auxIti]).fit(X)
+        labels  = db.labels_
+        indgood = db.labels_ != -1
+
+        ## ----- Salve the clusters and labels
+        Fcluster[db.labels_ == -1] = -1
+        Flabel[db.labels_   == -1] = 'n' # 'n' = noise points
+
+        if iterative == 0:
+            ## ----- Salve the clusters and labels
+            Fcluster        = labels
+            Flabel[indgood] = 's' # 'n' = noise points
+
+    if iterative >= 1:
+
+        Xnew      = X[indgood,:]
+        indicenew = np.where(indgood == True)[0]
+
+        auxIti    += 1
+        db         = DBSCAN(eps=vector_eps[auxIti], min_samples=vector_min_samples[auxIti]).fit(Xnew)
+        labels     = db.labels_
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+        # Find the Long clusters
+
+        clusters = [Xnew[labels == i] for i in range(n_clusters_)]
+
+        lenClu = np.zeros(np.size(clusters),)
+        for i in range(0,np.size(clusters)):
+            lenClu[i] = np.size(clusters[i])
+        clusterI = (np.where(lenClu > 900))[0]
+
+        if iterative == 1 or iterative == 4 or iterative == 12: # To salve ONLY the Long Clusters or 4 to all
+            ## ----- Salve the clusters and labels
+            for i in clusterI:
+                auxClu+=1
+                indice = Index[indicenew[labels == i]]
+                Fcluster[indice] = auxClu
+                Flabel[indice] = 'l' # 'l' = Long tracks
+
+    if iterative >= 2:
+
+        indgood2 = ~np.in1d(db.labels_, clusterI)
+        Xnew2 = Xnew[indgood2,:]
+        indicenew2 = np.where(indgood2 == True)[0]
+
+        auxIti+=1
+        db = DBSCAN(eps=vector_eps[auxIti], min_samples=vector_min_samples[auxIti]).fit(Xnew2)
+        labels = db.labels_
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+        clusters = [Xnew2[labels == i] for i in range(n_clusters_)]
+
+        lenClu = np.zeros(np.size(clusters),)
+        for i in range(0,np.size(clusters)):
+            lenClu[i] = np.size(clusters[i])
+        clusterI = (np.where(lenClu > 150))[0]
+
+        if iterative == 2 or iterative == 4 or iterative == 12: # To salve ONLY the Medium Clusters or 4 to all
+            ## ----- Salve the clusters and labels
+            for i in clusterI:
+                auxClu+=1
+                indice = Index[indicenew[indicenew2[labels == i]]]
+                Fcluster[indice] = auxClu
+                Flabel[indice] = 'm' # 'c' = Curly tracks
+
+
+    if iterative >= 3:
+
+        indgood3 = ~np.in1d(db.labels_, clusterI)
+        Xnew3 = Xnew2[indgood3,:]        
+        indicenew3 = np.where(indgood3 == True)[0]
+
+        auxIti+=1
+        db = DBSCAN(eps=vector_eps[auxIti], min_samples=vector_min_samples[auxIti]).fit(Xnew3)
+        labels = db.labels_
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+        clusters = [Xnew3[labels == i] for i in range(n_clusters_)]
+        
+        if iterative == 3 or iterative == 4: # To salve ONLY the Small Clusters or 4 to all
+            ## ----- Salve the clusters and labels
+            for j in range(0,np.size(clusters)):
+                auxClu+=1
+                indice = Index[indicenew[indicenew2[indicenew3[labels == j]]]]
+                Fcluster[indice] = auxClu
+                Flabel[indice] = 's' # 'c' = others tracks
+
+
+    return Fcluster, np.where(Fcluster != -1)[0], Flabel
+
+
+class iDBSCAN:
+    
+    def __init__(self, iterative = 3):
+        self.iterative = iterative
+
+    def fit(self, X):
+        
+        clust = idbscan(X, self.iterative)
+        self.labels_, self.core_sample_indices_, self.tag_  = clust
+        
+        return self
---
+++ toolslib.py
@@ -0,0 +1,101 @@
+import numpy as np
+import glob, os
+import re
+import sys
+
+
+def cluInfo(clusters,points,Ci,image,m_image,scale):
+    
+    labels = clusters.labels_
+    pos    = points[labels == Ci]
+    tag    = clusters.tag_[clusters.labels_== Ci][0]
+    Xi     = list(pos[:,0].astype(int))
+    Yi     = list(pos[:,1].astype(int))
+    
+    Is = []
+    Ib = []
+    for i in range(0, len(Xi)):    # loop on single cluster value
+        y0      = int(Xi[i]*scale)
+        x0      = int(Yi[i]*scale)
+        Is.extend(list(image[(y0):(y0+scale),(x0):(x0+scale)].reshape(scale*scale).astype(int)))
+        Ib.extend(list(m_image[(y0):(y0+scale),(x0):(x0+scale)].reshape(scale*scale).astype(int)))
+    
+    return Xi, Yi, Is, Ib, tag
+
+        
+def openTable(file):
+    with open(file) as f:
+        datain = eval(f.read())
+    return datain
+
+def saveTable(file,data):
+    with open(file, 'w') as f:
+        f.write(repr(data))
+        
+
+def colorbar(mappable):
+    # plot colorbars        
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+    ax = mappable.axes
+    fig = ax.figure
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    return fig.colorbar(mappable, cax=cax)
+
+def plot2hist(variable,bins,nsd,nse,label, density = True, logx = False, logy = False):
+    ## Function to show in the same histogram the three categories
+    
+    # variable = is a 3xN List which row has the information of one category       - List
+    # nins     = is the number of bins to construct the histogram                  - int
+    # nsd      = is the multiplication factor of the sigma to define the Xlim min  - float
+    # nse      = is the multiplication factor of the sigma to define the Xlim max  - float
+    # label    = is the Xlabel to show in the plot                                 - string
+    # density  = is the flag to set the histogram to show the density or not       - Boolean
+    # logx     = is the flag to set the X axis to log on the histogram or not      - Boolean
+    # logy     = is the flag to set the Y axis to log on the histogram or not      - Boolean
+    
+    import matplotlib.pyplot as plt
+    fig = plt.figure(figsize=(10,7))
+    ax = fig.add_subplot(111)
+    
+    v1 = np.append(variable[0],variable[1])
+    v2 = np.append(v1,variable[2])
+    
+    m=np.mean(v2[(v2 != 0) & (np.isnan(v2) == False)])
+    s=np.std(v2[(v2 != 0) & (np.isnan(v2) == False)])
+    
+    plt.hist(variable[0], bins=bins, range=(m-nsd*s, m+nse*s), fc='r', alpha = 0.7, density=density)
+    plt.hist(variable[1], bins=bins, range=(m-nsd*s, m+nse*s), fc='b', alpha = 0.7, density=density)
+    plt.hist(variable[2], bins=bins, range=(m-nsd*s, m+nse*s), fc='darkorange', alpha = 0.7, density=density)
+    
+    if logx:
+        plt.xscale("log")
+    if logy:
+        plt.yscale("log")
+    if density:
+        plt.ylabel('Probability')
+    else:
+        plt.ylabel('Counts')
+    plt.xlabel(label,fontsize=18)
+    plt.legend(['Recoils', 'Soft Electrons', 'MeV Electrons'],prop={'size': 18})
+    plt.show()
+    
+def getTaggedVariable(vari,col):
+    ## Function get the three categories on the same variable
+    #    INPUT    
+    # vari = is the DataFrame Pandas with all the information               - DataFrame
+    # col  = is the name of the coloumn wanted                              - String
+    #    OUTPUT
+    # v = is a 3xN List which row has the information of one category       - List
+    
+    v = [np.array(vari[col][vari.Tag == 'l']), np.array(vari[col][vari.Tag == 'm']), np.array(vari[col][vari.Tag == 's'])]
+    return v
+
+def plotMesh(X,Y,Z,el,graus):
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+    ax.view_init(elev=el, azim=graus)
+    plt.show()
+    return ax
